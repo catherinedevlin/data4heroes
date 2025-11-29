6922ff57e0a2cbfb972cd9216af24456ceb1ebc3
Catherine Devlin <catherine.devlin@gmail.com>
2025-11-29 06:13:18 -0500

    JSONL from HTML table

--- get-html.py
+++ get-html.py
@@ -1,9 +1,35 @@
-import pandas as pd 
+import sys
 
-URL = 'http://localhost:8000/ohio-supers.html'
+import jsonlines
+import pandas as pd
+
+URL = "http://localhost:8000/ohio-supers.html"
+
+def json_rows(row):
+    if row["Based in"]:
+        yield {
+            "entity_1": {
+                "type": "Person",
+                "name": row.Name,
+            },
+            "entity_2": {"type": "Place", "name": row["Based in"]},
+            "relationship": "based_in",
+        }
+    for power_num in range(1, 4):
+        field = f"Power {power_num}"
+        if row[field]:
+            yield {
+                "entity_1": {
+                    "type": "Person",
+                    "name": row.Name,
+                },
+                "entity_2": {"type": "Power", "name": row[field]},
+                "relationship": "has",
+            }
 
 frames = pd.read_html(URL)
-for (n, df) in enumerate(frames):
-    label = f" Frame {n} "
-    print(f"{label:=^60}")
-    print(df.head(3))
+df = frames[0].fillna('')
+with jsonlines.Writer(sys.stdout) as writer:
+    for idx, row in df.iterrows():
+        writer.write_all(json_rows(row))
+
